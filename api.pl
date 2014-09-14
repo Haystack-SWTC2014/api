4c0ec7eba7483de2a01eff6ce6c3e03deb9d86f1
Ben Tyler <benjamin.tyler@gmail.com>
2014-09-13 21:59:04 -0500

    add second endpoint

--- api.pl
+++ api.pl
@@ -4,12 +4,28 @@ use warnings;
 
 use Mojolicious::Lite;
 
-post '/synonyms' => sub {
+post '/domain' => sub {
+    my $c = shift;
+    my $query = $c->param('query');
+    my $output = `guess-domain $query`;
+    my $options = [split ',', $output];
+    $c->render(json => {
+        type => 'domain',
+        options => $options
+    });
+
+};
+
+post '/searches' => sub {
     my $c = shift;
     my $query = $c->param('query');
     my $domain = $c->param('domain');
-    my $output = `python improve-query.py $domain | $domain`;
-    $c->render(json => $output);
+    my $output = `python improve-query.py $domain | $query`;
+    my $options = [split ',', $output];
+    $c->render(json => {
+        type => 'search',
+        options => $options
+    });
 };
 
 app->start;
